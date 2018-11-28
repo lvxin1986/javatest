e950dc2d24f36bd92e33af44156800247018d7f0
bjlvxin <bjlvxin@jd.com>
2018-11-28 15:35:35 +0800

    add exchanger test

---
+++ src/main/java/test/concurrent/ExchangerTest.java
@@ -0,0 +1,48 @@
+import java.util.concurrent.Exchanger;
+
+class Car extends Thread {
+   private Exchanger<String> exchanger;
+
+   public Car(Exchanger<String> exchanger) {
+       super();
+       this.exchanger = exchanger;
+   }
+
+   @Override
+   public void run() {
+       try {
+           System.out.println(Thread.currentThread().getName() + ": " + exchanger.exchange("Car"));
+       } catch (InterruptedException e) {
+           e.printStackTrace();
+       }
+   }
+}
+
+ class Bike extends Thread {
+    private Exchanger<String> exchanger;
+
+    public Bike(Exchanger<String> exchanger) {
+        super();
+        this.exchanger = exchanger;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread().getName() + ": " + exchanger.exchange("Bike"));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class ExchangerTest {
+    public static void main(String[] args) {
+        Exchanger<String> exchanger = new Exchanger<String>();
+        Car car = new Car(exchanger);
+        Bike bike = new Bike(exchanger);
+        car.start();
+        bike.start();
+        System.out.println("Main end!");
+    }
+}
